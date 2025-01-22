294380ef2dfacf6a997e4d950f71c36b11762478
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-01-22 15:50:48 -0300

    Update database.php
    probando con otra base de datos

--- include/config/database.php
+++ include/config/database.php
@@ -3,7 +3,7 @@
 
 function conectarDB(): mysqli {
     $host = getenv('DB_HOST') ?: 'dpg-ctq6ofqj1k6c739pn1ng-a';
-    $user = getenv('DB_USER') ?: 'dbcontacto_user';
+    $user = getenv('DB_USER') ?: 'dbcontacto?user=dbcontacto_user&password';
     $password = getenv('DB_PASSWORD') ?: 'NmmS5wypyPKhdSZPkQCnPk4hb6toH1SJ';
     $database = getenv('DB_NAME') ?: 'dbcontacto';
 
