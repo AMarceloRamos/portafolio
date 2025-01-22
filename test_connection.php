440ecba9ba93bc212ba1996f525f190174ea7541
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-01-22 12:07:26 -0300

    test de conexion
    subiendo el teste de conexión

---
+++ test_connection.php
@@ -0,0 +1,14 @@
+<?php
+$host = getenv('DB_HOST') ?: 'dpg-ctq6ofqj1k6c739pn1ng-a';
+$user = getenv('DB_USER') ?: 'dbcontacto_user';
+$password = getenv('DB_PASSWORD') ?: 'NmmS5wypyPKhdSZPkQCnPk4hb6toH1SJ';
+$database = getenv('DB_NAME') ?: 'dbcontacto';
+
+$db = new mysqli($host, $user, $password, $database);
+
+if ($db->connect_error) {
+    die('Error no se puede conectar: ' . $db->connect_error);
+} else {
+    echo 'Conexión exitosa a la base de datos.';
+}
+?>
