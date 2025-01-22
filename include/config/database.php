5e99afe30ba27bb5465f017c186d8c4c03e66b33
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-01-22 16:44:09 -0300

    Update database.php
    simplificacion de la configuracion en le conexion

--- include/config/database.php
+++ include/config/database.php
@@ -5,30 +5,14 @@ function conectarDB(): mysqli {
     $password = getenv('DB_PASSWORD') ?: 'NmmS5wypyPKhdSZPkQCnPk4hb6toH1SJ';
     $database = getenv('DB_NAME') ?: 'dbcontacto';
 
-    $retries = 5; // Número de reintentos
-    $db = null;
+   
+    $db= new mysqli( $host, $user , $password ,  $database);
 
-    while ($retries > 0) {
-        try {
-            $db = new mysqli($host, $user, $password, $database);
-
-            // Comprueba si hay errores en la conexión
-            if ($db->connect_error) {
-                throw new Exception('Error de conexión: ' . $db->connect_error);
-            }
-
-            break; // Sal del bucle si la conexión es exitosa
-        } catch (Exception $e) {
-            $retries--;
-            error_log('Intento fallido de conexión: ' . $e->getMessage()); // Registro del error en el log
-            sleep(3); // Espera 3 segundos antes de reintentar
-        }
-    }
-
-    // Comprueba al final si la conexión falló después de los reintentos
-    if (!$db || $db->connect_error) {
-        die('Error no se puede conectar después de varios intentos: ' . ($db ? $db->connect_error : 'No se pudo inicializar mysqli'));
+    if(!$db){
+        die( "Error no se puede conectar". $db->connect_error);
+     
     }
 
     return $db;
+
 }
