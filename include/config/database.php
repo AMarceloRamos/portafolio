037480671e544f27ea8930ac1838928f3fd531f1
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-01-22 18:00:04 -0300

    Update database.php
    se cambio a Pdo

--- include/config/database.php
+++ include/config/database.php
@@ -1,17 +1,15 @@
 <?php
+
 function conectarDB() {
-    $host = "dpg-ctq6ofqj1k6c739pn1ng-a";
-    $port = "5432";
-    $dbname = "dbcontacto";
+    $dsn = "pgsql:host=dpg-ctq6ofqj1k6c739pn1ng-a;port=5432;dbname=dbcontacto;";
     $user = "dbcontacto_user";
     $password = "NmmS5wypyPKhdSZPkQCnPk4hb6toH1SJ";
 
-    $connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
-    $db = pg_connect($connection_string);
-
-    if (!$db) {
-        die("Error al conectar a la base de datos: " . pg_last_error());
+    try {
+        $db = new PDO($dsn, $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+    } catch (PDOException $e) {
+        die("Error al conectar a la base de datos: " . $e->getMessage());
     }
-
-    return $db;
 }
